c1940d7b4ad2ce97e059d2379e2cfca3e3b02e9d
Harrison Holt-McHale <hholtmchale@yozu.co.uk>
2018-09-27 12:31:09 +0100

    Finished 20

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[this.props.params.postId] || [];
     return (
       <div className="single-photo">
-        I'm Single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     )
   }
